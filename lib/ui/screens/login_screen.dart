4aea5b45699b75895bec1d91aae3dec7cabdcd6a
mynulyt <mynulalam27@gmail.com>
2025-10-30 00:00:51 +0600

    login screen with provider

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,10 +1,12 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:task_mngwithprovider/Data/model/user_model.dart';
 import 'package:task_mngwithprovider/Data/services/api_caller.dart';
 import 'package:task_mngwithprovider/Data/utils/urls.dart';
 import 'package:task_mngwithprovider/ui/controller/auth_controller.dart';
+import 'package:task_mngwithprovider/ui/controller/login_provider.dart';
 import 'package:task_mngwithprovider/ui/screens/forgot_password_verify_email_screen.dart';
 import 'package:task_mngwithprovider/ui/screens/main_nav_bar_holder_screen.dart';
 import 'package:task_mngwithprovider/ui/screens/sign_up_screen.dart';
@@ -26,99 +28,108 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _passwordTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  bool _loginInProgress = false;
+  final LoginProvider _loginProvider = LoginProvider();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ScreenBackground(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 82),
-                  Text(
-                    'Get Started With',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 24),
-                  TextFormField(
-                    controller: _emailTEController,
-                    decoration: InputDecoration(hintText: 'Email'),
-                    validator: (String? value) {
-                      String inputText = value ?? '';
-                      if (EmailValidator.validate(inputText) == false) {
-                        return 'Enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    controller: _passwordTEController,
-                    obscureText: true,
-                    decoration: InputDecoration(hintText: 'Password'),
-                    validator: (String? value) {
-                      if ((value?.length ?? 0) <= 6) {
-                        return 'Password should more than 6 letters';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  Visibility(
-                    visible: _loginInProgress == false,
-                    replacement: CenteredProgressIndecator(),
-                    child: FilledButton(
-                      onPressed: _onTapLoginButton,
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.purple,
-                        foregroundColor: Colors.white,
-                        padding: const EdgeInsets.all(12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: const Icon(Icons.arrow_circle_right_outlined),
+    return ChangeNotifierProvider(
+      create: (_) => _loginProvider,
+      child: Scaffold(
+        body: ScreenBackground(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 82),
+                    Text(
+                      'Get Started With',
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  const SizedBox(height: 36),
-                  Center(
-                    child: Column(
-                      children: [
-                        TextButton(
-                          onPressed: _onTapForgotPasswordButton,
-                          child: Text(
-                            'Forgot Password?',
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            text: "Don't have an account? ",
-                            children: [
-                              TextSpan(
-                                text: 'Sign up',
-                                style: TextStyle(color: Colors.green),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = _onTapSignUpButton,
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      controller: _emailTEController,
+                      decoration: InputDecoration(hintText: 'Email'),
+                      validator: (String? value) {
+                        String inputText = value ?? '';
+                        if (EmailValidator.validate(inputText) == false) {
+                          return 'Enter a valid email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    TextFormField(
+                      controller: _passwordTEController,
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: 'Password'),
+                      validator: (String? value) {
+                        if ((value?.length ?? 0) <= 6) {
+                          return 'Password should more than 6 letters';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    Consumer<LoginProvider>(
+                      builder: (context, loginProvider, _) {
+                        return Visibility(
+                          visible: loginProvider.loginInprogress == false,
+                          replacement: CenteredProgressIndecator(),
+                          child: FilledButton(
+                            onPressed: _onTapLoginButton,
+                            style: FilledButton.styleFrom(
+                              backgroundColor: Colors.purple,
+                              foregroundColor: Colors.white,
+                              padding: const EdgeInsets.all(12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            ],
+                            ),
+                            child: const Icon(
+                              Icons.arrow_circle_right_outlined,
+                            ),
                           ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 36),
+                    Center(
+                      child: Column(
+                        children: [
+                          TextButton(
+                            onPressed: _onTapForgotPasswordButton,
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              text: "Don't have an account? ",
+                              children: [
+                                TextSpan(
+                                  text: 'Sign up',
+                                  style: TextStyle(color: Colors.green),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = _onTapSignUpButton,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -150,31 +161,18 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Future<void> _login() async {
-    _loginInProgress = true;
-    setState(() {});
-    Map<String, dynamic> requestBody = {
-      "email": _emailTEController.text.trim(),
-      "password": _passwordTEController.text,
-    };
-    final ApiResponse response = await ApiCaller.postRequest(
-      url: Urls.loginUrl,
-      body: requestBody,
+    final bool isSuccess = await _loginProvider.login(
+      _emailTEController.text.trim(),
+      _passwordTEController.text,
     );
-    if (response.isSuccess && response.responseData['status'] == 'success') {
-      UserModel model = UserModel.fromJson(response.responseData['data']);
-      String token = response.responseData['token'];
-
-      await AuthController.saveUserData(model, token);
-
-      await Navigator.pushNamedAndRemoveUntil(
+    if (isSuccess) {
+      Navigator.pushNamedAndRemoveUntil(
         context,
         MainNavBarHolderScreen.name,
         (predicate) => false,
       );
     } else {
-      _loginInProgress = false;
-      setState(() {});
-      showSnackBarMessage(context, response.errorMessage!);
+      showSnackBarMessage(context, _loginProvider.errorMessage!);
     }
   }
 
