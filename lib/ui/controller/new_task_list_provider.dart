30e50304f58c5924a764c45c5be0de515c432a7d
mynulyt <mynulalam27@gmail.com>
2025-10-29 22:43:51 +0600

    new task list provider

--- lib/ui/controller/new_task_list_provider.dart
+++ lib/ui/controller/new_task_list_provider.dart
@@ -1,5 +1,45 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:task_mngwithprovider/Data/model/task_model.dart';
+
+import '../../data/services/api_caller.dart';
+import '../../data/utils/urls.dart';
 
 class NewTaskListProvider extends ChangeNotifier {
-  bool newTaskProgress = false;
+  bool _getNewTasksInProgress = false;
+
+  String? _errorMessage;
+
+  List<TaskModel> _newTaskList = [];
+
+  bool get getNewTasksProgress => _getNewTasksInProgress;
+
+  String? get errorMessage => _errorMessage;
+
+  List<TaskModel> get newTaskList => _newTaskList;
+
+  Future<bool> getNewTasks() async {
+    bool isSuccess = false;
+
+    _getNewTasksInProgress = true;
+    notifyListeners();
+
+    final ApiResponse response = await ApiCaller.getRequest(
+      url: Urls.newTaskListUrl,
+    );
+    if (response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.responseData['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _newTaskList = list;
+      isSuccess = true;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+
+    _getNewTasksInProgress = false;
+    notifyListeners();
+
+    return isSuccess;
+  }
 }
